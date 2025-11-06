ba05b5bcbeae8d51e324ab36aa3e0c8ea06c9aad
Ali Parham Ashrafzadeh <parham.jobb@gmail.com>
2025-11-06 20:34:29 +0100

    Add transcribe the voice from the cloud storage function

--- src/notetaker/core/speech_to_text/google_stt.py
+++ src/notetaker/core/speech_to_text/google_stt.py
@@ -1,33 +1,28 @@
-import io
 from google.oauth2 import service_account
-from google.cloud import storage
 from google.cloud import speech
 
 
-client_file = "src/notetaker/core/speech_to_text/stt_API2.json"
-credentials = service_account.Credentials.from_service_account_file(client_file)
-client = speech.SpeechClient(credentials=credentials)
+def transcribe_vioce():
+    client_file = "src/notetaker/core/speech_to_text/stt_API.json"
+    credentials = service_account.Credentials.from_service_account_file(client_file)
+    client = speech.SpeechClient(credentials=credentials)
 
-""" For less than 60 sec
-audio_file = "src/notetaker/core/speech_to_text/output.wav"
-with io.open(audio_file, "rb") as f:
-    content = f.read()
-    audio = speech.RecognitionAudio(content=content)
-"""
+    gs_uri = "gs://voice-data-bucket-12345/lecture_voice"
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code="en-US",
+        model="video",
+        enable_automatic_punctuation=True
+    )
 
-gcs_uri = "gcs:<uri link›"
-config = speech.RecognitionConfig(
-    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=16000,
-    language_code="en-US",
-    model="video"
-)
+    audio = speech.RecognitionAudio(uri=gs_uri)
+    operation = client.long_running_recognize(config=config, audio=audio)
 
-audio = speech.RecognitionAudio(uri=gcs_uri)
-operation = client.long_running_recognize(config=config, audio=audio)
 
-# response = client.recognize(config=config, audio=audio) ---> For less than 60 sec
+    print("Waiting for operation to complete...")
+    response = operation.result(timeout=90)
 
-print("Waiting for operation to complete...")
-response = operation. result(timeout=90)
-print(response)
+    full_text = " ".join(r.alternatives[0].transcript.strip() for r in response.results)
+
+    return full_text
