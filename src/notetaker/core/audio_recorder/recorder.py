475ae3c435e4d72ebbdfc5a82cf6a623d8558776
Ali Parham Ashrafzadeh <parham.jobb@gmail.com>
2025-11-06 15:18:43 +0100

    Add function for reusability to recorder.py

--- src/notetaker/core/audio_recorder/recorder.py
+++ src/notetaker/core/audio_recorder/recorder.py
@@ -2,41 +2,46 @@ import pyaudio
 import time
 import wave
 
-FRAMES_PER_BUFFER = 3200
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 16000
+def record_voice():
+    FRAMES_PER_BUFFER = 3200
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 16000
 
-p = pyaudio.PyAudio()
+    p = pyaudio.PyAudio()
 
-stream = p.open(
-    format=FORMAT,
-    channels=CHANNELS,
-    rate=RATE,
-    input=True,
-    frames_per_buffer=FRAMES_PER_BUFFER
-)
+    stream = p.open(
+        format=FORMAT,
+        channels=CHANNELS,
+        rate=RATE,
+        input=True,
+        frames_per_buffer=FRAMES_PER_BUFFER
+    )
 
-start = time.time()
-print("Start recording...")
+    start = time.time()
+    print("Start recording...")
 
-second = 5
-frames = []
-for i in range(0, int(RATE/FRAMES_PER_BUFFER*second)):
-    data = stream.read(FRAMES_PER_BUFFER)
-    frames.append(data)
+    second = 5
+    frames = []
+    for i in range(0, int(RATE/FRAMES_PER_BUFFER*second)):
+        data = stream.read(FRAMES_PER_BUFFER)
+        frames.append(data)
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
-end = time.time()
-length = end - start
-print(f"Recording stops: {length:.2f}")
+    end = time.time()
+    length = end - start
+    print(f"Recording stops: {length:.2f}")
 
-obj = wave.open("src/notetaker/core/data/output.wav", "wb")
-obj.setnchannels(CHANNELS)
-obj.setsampwidth(p.get_sample_size(FORMAT))
-obj.setframerate(RATE)
-obj.writeframes(b"".join(frames))
-obj.close()
+    obj = wave.open("src/notetaker/core/data/output.wav", "wb")
+    obj.setnchannels(CHANNELS)
+    obj.setsampwidth(p.get_sample_size(FORMAT))
+    obj.setframerate(RATE)
+    obj.writeframes(b"".join(frames))
+    obj.close()
+
+
+
+record_voice()
