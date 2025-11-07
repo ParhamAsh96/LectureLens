75058815354c58716c11eeee355191a2188b9158
Ali Parham Ashrafzadeh <parham.jobb@gmail.com>
2025-11-07 20:39:53 +0100

    Refactore the function cleaner

--- src/notetaker/core/cleaner/cleaner.py
+++ src/notetaker/core/cleaner/cleaner.py
@@ -1,28 +1,20 @@
 import os
 
-
 def clean_data():
-    voice_file = "src/notetaker/core/data/voice.wav"
-    transcript_file = "src/notetaker/core/data/transcript.json"
-    summary_file = "src/notetaker/core/data/summary.json"
+    paths = {
+        "audio": "src/notetaker/core/data/voice.wav",
+        "transcript": "src/notetaker/core/data/transcript.json",
+        "summary": "src/notetaker/core/data/summary.json",
+    }
 
-    try: 
-        os.remove(voice_file)
-        print(f"The audio file was deleted successfully.")
+    print("Removing old files...\n")
+    for name, path in paths.items():
+        try:
+            os.remove(path)
+            print(f"The {name} file was deleted successfully.\n")
 
-    except FileNotFoundError: 
-        print(f"The audio file was not found.")
-
-    try: 
-        os.remove(transcript_file)
-        print(f"The transcript file was deleted successfully.")
-
-    except FileNotFoundError: 
-        print(f"The transcript file was not found.")
-
-    try: 
-        os.remove(summary_file)
-        print(f"The summary file was deleted successfully.")
-
-    except FileNotFoundError: 
-        print(f"The summary file was not found.")
+        except FileNotFoundError:
+            print(f"The {name} file was not found.\n")
+        
+        except Exception as e:
+            print(f"Something went wrong: {e}")
