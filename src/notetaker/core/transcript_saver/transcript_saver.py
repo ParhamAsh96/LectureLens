2d95fda8347685f6ebc598fbbdddd994f67283a5
Ali Parham Ashrafzadeh <parham.jobb@gmail.com>
2025-11-07 04:55:41 +0100

    Add function saving transcript

--- src/notetaker/core/transcript_saver/transcript_saver.py
+++ src/notetaker/core/transcript_saver/transcript_saver.py
@@ -1,4 +1,19 @@
+import json
+from datetime import date
 from notetaker.core.speech_to_text.google_stt import transcribe_voice
 
 full_text = transcribe_voice()
 
+
+def save_transcript(full_text):
+    transcript = {
+        "Course": "Course name",
+        "Title": "Lecture x",
+        "Description": full_text,
+        "Date": date.today().strftime("%d-%m-%Y")
+    }
+
+    with open("src/notetaker/core/data/transcript.json", "w", encoding="utf-8") as file:
+        json.dump(transcript, file, ensure_ascii=False, indent=2)
+
+save_transcript(full_text)
